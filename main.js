b79909031bd02a96498adc8c27bb66b1a08cf6e4
Kucing7676 <faizalaliapriyono@gmail.com>
2020-12-20 17:54:21 +0700

    deploy1

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {    
+    let nav_offset_top = $('.carousel-item').height()-999;
+    
+        function navbarFixed() {
+            if ($('.carousel').length) {
+                $(window).scroll(function() {
+                    let scroll = $(window).scrollTop();
+                    if (scroll >= nav_offset_top) {
+                        $('.navbar').addClass('fixed-top navscroll');
+                    } else {
+                        $('.navbar').removeClass('fixed-top navscroll');
+                    }
+                })
+            }
+        }
+        navbarFixed();
+    })
